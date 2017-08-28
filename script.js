415bd63f79946c631aad5976db7cc99f67e6c9cb
dane <dane@qburst.com>
2017-08-28 15:09:54 +0530

    Models can be added.

---
+++ script.js
@@ -0,0 +1,74 @@
+/* script file for index.html */
+
+var app = {};
+
+// Model
+app.TodoModel = Backbone.Model.extend({
+  defaults: {
+    heading: '',
+    content: '',
+    author: '',
+    status: 'todo'
+  }
+});
+
+// Collection
+app.TodoCollection = Backbone.Collection.extend({
+  model: app.TodoModel,
+  localStorage: new Store("real-todo")
+});
+
+app.todoList = new app.TodoCollection();
+app.progressList = new app.TodoCollection();
+app.reviewList = new app.TodoCollection();
+app.doneList = new app.TodoCollection();
+
+app.ItemView = Backbone.View.extend({
+  tagName: 'li',
+  itemTemplate: _.template($("#childTemplate").html()),
+  render: function () {
+    this.$el.addClass("collection-item");
+    this.$el.html(this.itemTemplate(this.model.toJSON()));
+    return this;
+  }
+});
+
+app.TodoView = Backbone.View.extend({
+  el: '#input-todo',
+  initialize: function () {
+    app.todoList.on("add", this.addOne, this);
+    app.todoList.fetch();
+  },
+  events: {
+    'keypress #new-author': 'addNewTodoOnEnter',
+    'click #click-todo': 'addNewTodoOnClick'
+  },
+  addNewTodoOnEnter: function (e) {
+    if( e.which !== 13 ) {
+      return;
+    }
+    app.todoList.create(this.newTodo());
+    $("#new-heading").val('');
+    $("#new-task").val('');
+    $("#new-author").val('');
+  },
+  addNewTodoOnClick: function () {
+    app.todoList.create(this.newTodo());
+    $("#new-heading").val('');
+    $("#new-task").val('');
+    $("#new-author").val('');
+  },
+  addOne: function (item) {
+    let itemView = new app.ItemView({model: item});
+    $("#todo-list").append(itemView.render().el);
+  },
+  newTodo: function () {
+    return {
+      heading: $("#new-heading").val().trim(),
+      content: $("#new-task").val().trim(),
+      author: $("#new-author").val().trim()
+    };
+  }
+});
+
+var todoView = new app.TodoView();
